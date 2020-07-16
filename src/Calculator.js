181cd528d58f7918b78edefffd9ec63f67a3f4f7
Rewe <kewyn.ryan@gmail.com>
2020-07-16 18:36:14 -0300

    Calculator is now pretty much functional

--- src/Calculator.js
+++ src/Calculator.js
@@ -8,9 +8,6 @@ class Calculator extends React.Component
         {
             'text': '0',
         }
-        
-        this.numbers = [];
-        this.operators = [];
     }
     
     getCalcState()
@@ -21,59 +18,96 @@ class Calculator extends React.Component
 
     setCalcState(state)
     {
-        let rawText = state.text;
-
-        if (rawText.charAt(1) && rawText.charAt(0) == 0)
-            rawText = rawText.slice(1);
-
-            rawText = rawText.split('');
-
-        if
-        (
-            rawText[rawText.length -1] == '+' ||
-            rawText[rawText.length -1] == '-' ||
-            rawText[rawText.length -1] == '*' ||
-            rawText[rawText.length -1] == '/' ||
-            rawText[rawText.length -1] == '^'
-        )
-        {
-            this.operators.pop(rawText.splice(-1));
-            console.log(rawText);
-        }
-
-        let text;
-
-        if(rawText.length)
-            for(let i = 0; i < rawText.length; i++)
-            {
-                if(!text)
-                    text = rawText[i];
-                else
-                    text += rawText[i];
-            }
-        else
-            text = '0';
-        
-        this.numbers.pop(Number(text));
-        console.log(text, rawText);
-
-        state.text = text;
+        if(state.text == '')
+            state.text = '0';
         this.setState(state);
     }
 
-    calculate()
+    addChar(char)
     {
-        let result;
-        let numbers = this.numbers;
-        let operators = this.operators;
         let state = this.getCalcState();
+        let regExpN = /[0-9.]/;
+        let regExpO = /[+*/^]|-/;
+
+        if(char.match(regExpN))
+        {
+            if(state.text.length == 1 && state.text.charAt(0) == '0')
+                state.text = char;
+            else
+                state.text += char;
+        }
+        else if(char.match(regExpO))
+        {
+            if(state.text.charAt(state.text.length -1) == '.')
+                state.text = state.text.slice(0, -1);
+
+            state.text += ` ${char} `;
+        }
+
+        if(char == '√')
+            state.text += ` ^ 0.5`;
+        this.setCalcState(state);
+    }
+
+    parseString()
+    {
+        let rawText = this.getCalcState().text;
+        let numbers = [];
+        let operators = [];
+        let regExpN = /[0-9.]/;
+        let regExpO = /[+*/^]|-/;
+        console.log('parseString');
+        console.log('rawText: ', rawText);
+
+        rawText = rawText.split(' ');
+        
+        for(let i = 0; i < rawText.length; i++)
+        {
+            if(rawText[i].match(regExpN))
+            {
+                numbers.push(Number(rawText[i]));
+            }
+            
+            if(rawText[i].match(regExpO))
+            {
+                operators.push(rawText[i]);
+            }
+        }
+        console.log('rawText2', rawText);
+        
+        console.log('numbers: ',numbers);
+        console.log('operators: ',operators);
+        console.log('');
+
+        return {numbers: numbers, operators: operators}
+    }
+
+    getResult()
+    {
+        console.log('calculate');
+        let result;
+        let parsedStr = this.parseString()
+        let numbers = parsedStr.numbers;
+        let operators = parsedStr.operators;
+
+        console.log('numbers2: ', numbers);
+        console.log('operators2: ', operators);
 
         for(let i = 0; i < operators.length; i++)
         {
             if(operators[i] == '^')
             {
-                numbers[i -1] = Math.pow(numbers[i-1], numbers[i]);
-                numbers.splice(i, i+1);
+                if(numbers[i] != 0 && numbers[i +1] != 0)
+                {
+                    numbers[i] = Math.pow(numbers[i], numbers[i +1]);
+                    operators.splice(i, 1);
+                    numbers.splice(i +1, 1);
+                    i--;
+                }
+                else
+                {
+                    return 'Zero to the power of Zero is undefined';
+                }
             }
 
         }
@@ -83,53 +117,120 @@ class Calculator extends React.Component
             
             if(operators[i] == '*')
             {
-                numbers[i -1] = numbers[i-1] * numbers[i];
-                numbers.splice(i, i+1);
+                numbers[i] = numbers[i] * numbers[i +1];
+                operators.splice(i, 1);
+                numbers.splice(i +1, 1);
+                i--;
             }
 
             if(operators[i] == '/')
             {
-                numbers[i -1] = numbers[i-1] / numbers[i];
-                numbers.splice(i, i+1);
+                if(numbers[i +1] != 0)
+                {
+                    numbers[i] = numbers[i] / numbers[i +1];
+                    operators.splice(i, 1);
+                    numbers.splice(i +1, 1);
+                    i--;
+                }
+                else
+                {
+                    return 'Dividing by zero is undefined';
+                }
             }
 
         }
 
         for(let i = 0; i < operators.length; i++)
         {
+            
+            console.log('numbers3: ', numbers);
+            console.log('operators3: ', operators);
             if(operators[i] == '+')
             {
-                numbers[i -1] = numbers[i-1] + numbers[i];
-                numbers.splice(i, i+1);
+                numbers[i] = numbers[i] + numbers[i +1];
+                operators.splice(i, 1);
+                numbers.splice(i +1, 1);
+                i--;
             }
+            
+            console.log('numbers4: ', numbers);
+            console.log('operators4: ', operators);
 
             if(operators[i] == '-')
             {
-                numbers[i -1] = numbers[i-1] - numbers[i];
-                numbers.splice(i, i+1);
+                numbers[i] = numbers[i] - numbers[i +1];
+                operators.splice(i, 1);
+                numbers.splice(i +1, 1);
+                i--;
             }
         }
-        
 
-        if(result)
+        if(numbers.length == 1 && !operators.length)
         {
-            this.numbers = [];
-            this.operators = [];
-            state.text = result;
-            this.setCalcState(state);
-            console.log(result);
-            return result;
+            if((numbers[0] || numbers[0] == 0) && typeof(numbers[0] == 'number'))
+            {
+                result = numbers[0];
+                console.log('result: ', result);
+                console.log('');
+                return result;
+            }
+            else
+            {
+                if(numbers[0] == undefined)
+                    console.log('Result is ', numbers[0]);
+
+                return 'Error';
+            }
         }
         else
         {
-            // Throw error
-            return -1;
+            if(!numbers.length)
+                console.log('There are no numbers left');
+
+            if(numbers.length > 1)
+                console.log('There is more than one number left');
+
+            if(operators.length)
+                console.log('There still are operators left');
+
+            return 'Error';
         }
     }
 
-    UNSAFE_componentWillUpdate()
+    calculate()
     {
-        return;
+        let result = this.getResult();
+
+        if(result == 'Error')
+            return -1;
+        else
+            this.changeText(String(result));
+    }
+
+    changeText(string)
+    {
+        let state = this.getCalcState();
+
+        state.text = string;
+        this.setCalcState(state);
+    }
+
+    clearText()
+    {
+        this.changeText('0');
+    }
+
+    backspace()
+    {
+        let text = this.getCalcState().text;
+
+        if(text.charAt(text.length -1) == ' ')
+            text = text.slice(0, -1);
+        text = text.slice(0, -1);
+        if(text.charAt(text.length -1) == ' ')
+            text = text.slice(0, -1);
+
+        this.changeText(text);
     }
 
     render()
@@ -139,43 +240,50 @@ class Calculator extends React.Component
 
                 <div className='Holder'>
                     <div id='Display'>
-                        <span id='HigherLED'></span>
                         <span id='LED'>{this.state.text}</span>
+                        <span id='LowerLED'>{`${this.getResult()}`}</span>
                     </div>
                 </div>
 
                 <div className='Holder'>
-                    <Key text={'^2'} getCalcState={() => this.getCalcState()} setCalcState={(state) => this.setCalcState(state)}></Key>
-                    <Key text={'^1/2'} getCalcState={() => this.getCalcState()} setCalcState={(state) => this.setCalcState(state)}></Key>
-                    <Key text={'e'} getCalcState={() => this.getCalcState()} setCalcState={(state) => this.setCalcState(state)}></Key>
-                    <Key text={' *'} getCalcState={() => this.getCalcState()} setCalcState={(state) => this.setCalcState(state)}></Key>
+                    <Key text={'C'}  onClick={ () => this.clearText()}></Key>
+                    <Key text={'^x'}  onClick={ () => this.addChar('^')}></Key>
+                    <Key text={'2√'}  onClick={ () => this.addChar('√')}></Key>
+                    <Key text={'←'}  onClick={ () => this.backspace()}></Key>
                 </div>
 
                 <div className='Holder'>
-                    <Key text={'7'} getCalcState={() => this.getCalcState()} setCalcState={(state) => this.setCalcState(state)}></Key>
-                    <Key text={'8'} getCalcState={() => this.getCalcState()} setCalcState={(state) => this.setCalcState(state)}></Key>
-                    <Key text={'9'} getCalcState={() => this.getCalcState()} setCalcState={(state) => this.setCalcState(state)}></Key>
-                    <Key text={' /'} getCalcState={() => this.getCalcState()} setCalcState={(state) => this.setCalcState(state)}></Key>
+                    <Key text={'+'}  onClick={ () => this.addChar('+')}></Key>
+                    <Key text={'-'}  onClick={ () => this.addChar('-')}></Key>
+                    <Key text={'/'}  onClick={ () => this.addChar('/')}></Key>
+                    <Key text={'*'}  onClick={ () => this.addChar('*')}></Key>
                 </div>
 
                 <div className='Holder'>
-                    <Key text={'4'} getCalcState={() => this.getCalcState()} setCalcState={(state) => this.setCalcState(state)}></Key>
-                    <Key text={'5'} getCalcState={() => this.getCalcState()} setCalcState={(state) => this.setCalcState(state)}></Key>
-                    <Key text={'6'} getCalcState={() => this.getCalcState()} setCalcState={(state) => this.setCalcState(state)}></Key>
-                    <Key text={' +'} getCalcState={() => this.getCalcState()} setCalcState={(state) => this.setCalcState(state)}></Key>
+                    <Key text={'7'}  onClick={ () => this.addChar('7')}></Key>
+                    <Key text={'8'}  onClick={ () => this.addChar('8')}></Key>
+                    <Key text={'9'}  onClick={ () => this.addChar('9')}></Key>
+                    <Key text={''}  onClick={ () => {}}></Key>
                 </div>
 
                 <div className='Holder'>
-                    <Key text={'1'} getCalcState={() => this.getCalcState()} setCalcState={(state) => this.setCalcState(state)}></Key>
-                    <Key text={'2'} getCalcState={() => this.getCalcState()} setCalcState={(state) => this.setCalcState(state)}></Key>
-                    <Key text={'3'} getCalcState={() => this.getCalcState()} setCalcState={(state) => this.setCalcState(state)}></Key>
-                    <Key text={' -'} getCalcState={() => this.getCalcState()} setCalcState={(state) => this.setCalcState(state)}></Key>
+                    <Key text={'4'}  onClick={ () => this.addChar('4')}></Key>
+                    <Key text={'5'}  onClick={ () => this.addChar('5')}></Key>
+                    <Key text={'6'}  onClick={ () => this.addChar('6')}></Key>
+                    <Key text={''}  onClick={ () => {}}></Key>
                 </div>
 
                 <div className='Holder'>
-                    <Key text={'.'} getCalcState={() => this.getCalcState()} setCalcState={(state) => this.setCalcState(state)}></Key>
-                    <Key text={'0'} getCalcState={() => this.getCalcState()} setCalcState={(state) => this.setCalcState(state)}></Key>
-                    <Key text={' ='} isHor={true} getCalcState={() => this.getCalcState()} setCalcState={(state) => this.calculate}></Key>
+                    <Key text={'1'}  onClick={ () => this.addChar('1')}></Key>
+                    <Key text={'2'}  onClick={ () => this.addChar('2')}></Key>
+                    <Key text={'3'}  onClick={ () => this.addChar('3')}></Key>
+                    <Key text={''}  onClick={ () => {}}></Key>
+                </div>
+
+                <div className='Holder'>
+                    <Key text={'.'}  onClick={ () => this.addChar('.')}></Key>
+                    <Key text={'0'}  onClick={ () => this.addChar('0')}></Key>
+                    <Key text={'='} isHor={true} onClick={ () => this.calculate()}></Key>
                 </div>
 
             </div>
@@ -189,26 +297,13 @@ class Key extends React.Component
     {
         super(props);
 
-        this.getCalcState = this.props.getCalcState;
-        this.setCalcState = this.props.setCalcState;
-    }
-
-    addChar()
-    {
-        let state = this.getCalcState();
-        state.text = state.text + this.props.text;
-        this.setCalcState(state);
+        this.onClick = this.props.onClick;
     }
 
     render()
     {
         return (
-            <div className='Key' onClick={() => this.addChar()}>
-                {/*
-
-                    COLOCAR onClick em props.
-
-                */}
+            <div className='Key' onClick={() => this.onClick()}>
                 {
                     this.props.isHor == true &&
                         <span className='Horizontal'>{this.props.text}</span>
